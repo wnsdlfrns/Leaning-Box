7f03c28864ff8c5d79e1c4a37e19a301c012977a
jl92 <62494034+wnsdlfrns@users.noreply.github.com>
2025-01-22 10:22:38 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -3,15 +3,8 @@ import sys
 n, m = map(int, sys.stdin.readline().split())
 list_data = list(range(1, n+1))
 
-# print(list_data)
-
 for _ in range(m):
   x, y = map(int, sys.stdin.readline().split())
-  # print(list_data[x:y])
-  # print(list_data[x:y+1])
-  # print(list_data[x:y+1:-1])
-  # print(list_data[x:y+1][::-1])
   list_data[x-1:y] = list_data[x-1:y][::-1]
-  # print(list_data)
-  
+
 print(*list_data)
